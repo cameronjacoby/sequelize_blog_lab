b3a3f966a3cd2b79a8f369a0475d9b32834916de
cameronjacoby <cameron.jacoby@gmail.com>
2014-07-30 23:16:56 -0700

    working sign up & log in pages

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express'),
-  db = require('./models/index'),
   ejs = require('ejs'),
   bodyParser = require('body-parser'),
-  app = express();
+  passport = require('passport'),
+  passportLocal = require('passport-local'),
+  app = express(),
+  db = require('./models/index');
 
 
 app.set('view engine', 'ejs');
@@ -13,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 // renders home page (shows all blog posts)
 app.get('/blog', function(req, res) {
   db.post.findAll().success(function(posts) {
-    res.render('site/index', {posts: posts, username: ''});
+    res.render('site/index', {posts: posts});
   });
 });
 
@@ -48,13 +50,13 @@ app.get('/user/:id', function(req, res) {
 
 // renders signup page
 app.get('/signup', function(req, res) {
-  res.render('site/signup', {username: ''});
+  res.render('site/signup', {username: '', message: ''});
 });
 
 
 // renders login page
 app.get('/login', function(req, res) {
-  res.render('site/login', {username: ''});
+  res.render('site/login', {username: '', message: ''});
 });
 
 
@@ -64,13 +66,42 @@ app.post('/blog', function(req, res) {
   var title = req.body.post.title;
   var content = req.body.post.content;
 
-  db.user.findOrCreate({username: username, password: 'password'}).success(function(user) {
+  db.user.find({where: {username: username}}).success(function(user) {
     db.post.create({title: title, content: content}).success(function(newPost) {
       user.addPost(newPost).success(function() {
         res.redirect('/blog');
       });
     });
   });
+});
+
+
+// create new user upon sign up
+app.post('/signup', function(req, res) {
+  newUsername = req.body.username;
+  newPassword = req.body.password;
+  db.user.createNewUser(newUsername, newPassword,
+    function(err) {
+      res.render('site/signup', {message: err.message, username: newUsername});
+    },
+    function(success) {
+      res.render('posts/new', {message: success.message});
+    }
+  );
+});
+
+
+app.post('/login', function(req, res) {
+  enteredUsername = req.body.username;
+  enteredPassword = req.body.password;
+  db.user.authorizeUser(enteredUsername, enteredPassword,
+    function(err) {
+      res.render('site/login', {message: err.message, username: enteredUsername});
+    },
+    function(success) {
+      res.render('posts/new', {message: 'Successfully signed in! Now you can add a new blog post.'});
+    }
+  );
 });
 
 
--- models/user.js
+++ models/user.js
@@ -1,3 +1,8 @@
+var bcrypt = require('bcrypt');
+var salt = bcrypt.genSaltSync(10);
+var passport = require('passport');
+var passportLocal = require('passport-local');
+
 function User(sequelize, DataTypes) {
   var User = sequelize.define('user', {
     username: {
@@ -14,8 +19,71 @@ function User(sequelize, DataTypes) {
         notEmpty: true
       }
     }
-  });
+  },
+    {
+      classMethods: {
+        // encrypt a password
+        encryptPass: function(password) {
+          var hash = bcrypt.hashSync(password, salt);
+          return hash;
+        },
+        // compare a password
+        comparePass: function(userpass, dbpass) {
+          return bcrypt.compareSync(userpass, dbpass);
+        },
+        // create a new user
+        createNewUser: function(username, password, err, success) {
+          if (password.length < 6) {
+            err({message: 'Your password should be more than 6 characters.'});
+          }
+          else {
+            User.create({
+              username: username,
+              password: User.encryptPass(password)
+            }).error(function(error) {
+              console.log(error);
+              if (error.username) {
+                err({message: 'Your username should be at least 6 characters.'});
+              }
+              else {
+                err({message: 'An account with that username already exists.'});
+              }
+            }).success(function(user) {
+              success({message: 'Successfully signed up! Now you can add a new blog post.'});
+            });
+          }
+        },
+        // authorize a user
+        authorizeUser: function(username, password, err, success) {
+          User.find({
+            where: {
+              username: username
+            }
+          })
+          // when that's done...
+          .done(function(error, user) {
+            if (error) {
+              console.log(error);
+              err({message: 'Oops! Something went wrong on our end.'});
+            }
+            else if (user === null) {
+              err({message: 'Username does not exist.'});
+            }
+            else if (User.comparePass(password, user.password) === true) {
+              success();
+            }
+            else {
+              err({message: 'Invalid password.'});
+            }
+          });
+        }
+      }
+    });
   return User;
 }
 
 module.exports = User;
+
+
+
+
